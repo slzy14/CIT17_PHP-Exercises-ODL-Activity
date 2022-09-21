178cd7d46f72daa13672196bb10d552ea4645e28
unknown <akikiun14@gmail.com>
2022-09-21 12:10:55 +0800

    initial upload

--- Table.php
+++ Table.php
@@ -1,12 +1,11 @@
 <?php
-echo "<table border =\"1\" style='border-collapse: collapse'>";
 	for ($row=1; $row <= 7; $row++) { 
-		echo "<tr> \n";
+		echo "   \n";
 		for ($col=1; $col <= 7; $col++) { 
 		   $p = $col * $row;
-		   echo "<td>$p</td> \n";
+		   echo $p ."   ";
 		   	}
-	  	    echo "</tr>";
+	  	  
 		}
-		echo "</table>";
+	
 ?>
